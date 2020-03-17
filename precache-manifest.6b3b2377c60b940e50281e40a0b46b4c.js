9045af78c55bde07cc65d0ead3870fc156e2e05d
nanogrunge <gdg007@gmail.com>
2020-03-17 19:56:36 -0300

    Updates

---
+++ precache-manifest.6b3b2377c60b940e50281e40a0b46b4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9737e2ea2ff799e4fd076a1fef74cc04",
+    "url": "/reactcrud/index.html"
+  },
+  {
+    "revision": "06d92a84ccfff245bbd4",
+    "url": "/reactcrud/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8b88d283b25f0a42df00",
+    "url": "/reactcrud/static/js/2.9f511dba.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactcrud/static/js/2.9f511dba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d92a84ccfff245bbd4",
+    "url": "/reactcrud/static/js/main.d3d7c06d.chunk.js"
+  },
+  {
+    "revision": "cfd5a57ada1883e02049",
+    "url": "/reactcrud/static/js/runtime-main.f40bf395.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactcrud/static/media/logo.5d5d9eef.svg"
+  }
+]);
